6b7e3adc599be057a93a431378d9aff3e73b6366
SeoNaru <tjehddnr12345@naver.com>
2023-12-07 17:25:32 +0900

    Preparing for Server Deployment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors'; // cors 모듈 추가
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    // 1. CORS 설정 추가
+    app.use(
+      cors({
+        origin: ['http://localhost:3060', 'http://52.78.104.66'],
+        credentials: true,
+      }),
+    );
+
+    // 2. 포트 번호 변경 및 서버 시작
+    await app.listen(80, () => {
+      console.log('서버 실행 중!');
+    });
+  } catch (error) {
+    console.error('Error during application bootstrap:', error);
+  }
 }
+
 bootstrap();
