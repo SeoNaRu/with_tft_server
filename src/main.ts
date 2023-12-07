8140f9c168e43714c1c9589a384ac036f01af30f
SeoNaru <tjehddnr12345@naver.com>
2023-12-07 19:28:02 +0900

    Updated server code to listen on port 8080 for external access in Docker container.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    // 0.0.0.0은 모든 IP에서 접속을 허용하는 의미입니다.
+    await app.listen(8080, '0.0.0.0');
+    console.log(`Application is running on: http://localhost:8080`);
   } catch (error) {
     console.error('Error during application bootstrap:', error);
   }
