08fc57132acc01c9ac97d27c0fc89de8019594ab
SeoNaru <tjehddnr12345@naver.com>
2023-12-07 19:56:23 +0900

    feat docker-compose

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     // 0.0.0.0은 모든 IP에서 접속을 허용하는 의미입니다.
-    await app.listen(8080, '0.0.0.0');
+    await app.listen(8080);
     console.log(`Application is running on: http://localhost:8080`);
   } catch (error) {
     console.error('Error during application bootstrap:', error);
