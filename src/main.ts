8fc569f2d1f2da6004c226cf31c2661649b594cc
SeoNaru <tjehddnr12345@naver.com>
2023-12-11 19:18:22 +0900

    feet:Enable CORS for external access

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    // 0.0.0.0은 모든 IP에서 접속을 허용하는 의미입니다.
+    app.enableCors();
     await app.listen(8080);
     console.log(`Application is running on: http://localhost:8080`);
   } catch (error) {
